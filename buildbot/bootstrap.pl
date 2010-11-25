55ade0ddad2688d3b3f54c962b149ceb76c44b5d
Asher Baker <asherkin@limetech.org>
2010-11-25 14:45:22 +0700

    more perl hate

--- buildbot/bootstrap.pl
+++ buildbot/bootstrap.pl
@@ -51,7 +51,7 @@ if ($reconf) {
 		my @sdks = ('sourcemod-1.3', 'mmsource-1.8', 'hl2sdk', 'hl2sdk-ob', 'hl2sdk-ob-valve', 'hl2sdk-l4d', 'hl2sdk-l4d2');
 		my ($sdk);
 		foreach $sdk (@sdks) {
-			print 'Updating checkout of ', $sdk, ' on ' $^O, '\n';
+			print 'Updating checkout of ', $sdk, ' on ', $^O, '\n';
 			$result = `hg pull -u /home/builds/common/$sdk`;
 			print $result;
 		}
@@ -68,7 +68,7 @@ if ($reconf) {
 		my @sdks = ('sourcemod-1.3', 'mmsource-1.8', 'hl2sdk-ob-valve');
 		my ($sdk);
 		foreach $sdk (@sdks) {
-			print 'Updating checkout of ', $sdk, ' on ' $^O, '\n';
+			print 'Updating checkout of ', $sdk, ' on ', $^O, '\n';
 			$result = `hg pull -u /Users/builds/builds/common/$sdk`;
 			print $result;
 		}
@@ -81,7 +81,7 @@ if ($reconf) {
 		my @sdks = ('sourcemod-1.3', 'mmsource-1.8', 'hl2sdk', 'hl2sdk-darkm', 'hl2sdk-ob', 'hl2sdk-ob-valve', 'hl2sdk-l4d', 'hl2sdk-l4d2', 'hl2sdk-swarm');
 		my ($sdk);
 		foreach $sdk (@sdks) {
-			print 'Updating checkout of ', $sdk, ' on ' $^O, '\n';
+			print 'Updating checkout of ', $sdk, ' on ', $^O, '\n';
 			$result = `hg pull -u C:/Scripts/common/$sdk`;
 			print $result;
 		}
